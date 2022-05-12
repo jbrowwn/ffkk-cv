1e3e06926ce6b2b9e987f435815663fc277bf415
jbrowwn <105454867+jbrowwn@users.noreply.github.com>
2022-05-12 20:08:16 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+
+import streamlit as st
+
+def cover_letter() -> None:
+    """
+    This page will hold the cover letter for the FCA Senior Data Scientist Role.
+    """
+
+    st.write(
+        """
+        # SearchLight Parser (SLiP TOOL)
+        Hi! Welcome to our tool! :wave:
+        
+        ## Cover Letter, Senior Associate Data Scientist
+
+        I am applying for the role of Senior Associate Data Scientist within the FCA. I am a Data Scientist currently within the Social Data
+        Science Unit, with previous work in Enforcement-DSU and FPET- (formally JSET-) DSU. A signifcant reason for me initially joining the FCA,
+        and thus a reason I am seeking to progress, is due to the purpose of the organisation. Our collective mission is relatively broad, however,
+        I personally believe successfully protecting consumers from harm, promoting fair competition, or ensuring market integrity are challenging 
+        yet rewarding aims which attract me and provide meaning to my work.
+
+        Throughout my years of coding and data science work, I have been greatly fortunate to have incredible opportunities to lead and contribute on many 
+        data science projects which have been of genuine use to my stakeholders and help the FCA achieve our mission. As detailed on my CV, I have had experience
+        with:
+
+        -- NLP 
+
+
+
+
+
+
+
+
+
+        I am in the final year of my geography degree, expecting a 2:1. Always intending to have a career in business, 
+        I have taken modules on the geography of business and GIS modelling. My final-year dissertation is on changing patterns in retail. 
+        During my degree, I have developed my analytical skills and ability to read, manage and present data. I have also become familiar 
+        with a range of business intelligence sources.
+
+        As you can see from my CV I have experience in:
+
+        Retail - moving from shelf stacker to checkout operator to team leader in my two years with Fresh Foods. I contributed to the store
+        consistently being in the top five for the region by providing excellent customer service.
+        Warehouse operations - picking and packing to meet targets over the busy Christmas period.
+        
+        
+        
+        
+        As evidenced as an internal candidate, I greatly believe
+        in the mission of the FCA. Although at times there are challenges in achieving our objectives with maximal efficacy, the projects I have worked on
+        (detailed throughout my CV) in which I have successfully helped ensure one or more of ensuring that markets operate with integrity; 
+        promoting effective competition; or protecting consumers of financial services has been personally rewarding. 
+
+        To prove my value for the role, I will break down the role title into two distinct parts - technical and leadership. Specifics of each are covered in
+        my CV, so I will provide context here to supplement. 
+
+        For "technical", my data science ability has been evidenced in multiple projects, however I will focus on two here. The first one is a tool I built 
+        in the FPET (formerly JSET) department - the Searchlight Parser Tool (hereafter: SliP). As the name suggests, the tool is built from the outputs of Advanced 
+        Analytics' Searchlight tool. The outputs of SliP was originally built to the specific requirements of FPET stakeholders - a prime example of the "hub and 
+        spoke model" we have here at the FCA (between AA and the business-integrated DSUs). I built SliP to solve a vital business problem with an enormous amount of
+        manual overhead. 
+
+
+        """
+    )
+
+cover_letter()
